fe163df45b975960d8b6fba39aab1f0d943cc0df
Thomas Colin <thomas.colin.prestataire@wiztivi.com>
2017-09-14 16:44:40 +0200

    Add year and flags parsing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const Rules = require('./rules.js')
-const properties = ['language', 'source', 'encoding', 'resolution', 'dub'] // missing 'year', 'flags' (multi), 'type', 'group' and 'title'
+const properties = ['language', 'source', 'encoding', 'resolution', 'dub', 'year', 'flags'] // missing 'type', 'group' and 'title'
 
 class Release {
   constructor(name, strict = true, defaults = {}) {
@@ -15,7 +15,7 @@ class Release {
     this.cleaned = cleaned
 
     properties.map(property => {
-      const result = this.parse(property, waste)
+      const result = this.parse(property, waste, (property === 'flags'))
 
       if (result.match || defaults[property]) {
         this[property] = result.match || defaults[property]
@@ -29,30 +29,49 @@ class Release {
     return name.replace(/[\[\]\(\)\;\:\!\s\\]+/g, '.')
   }
 
-  parse(property, name) {
-    const rule = Rules[property]
-    const tags = Object.keys(rule)
-
-    for (let i = 0; i < tags.length; i++) {
-      const tag = tags[i]
-      const patterns = (Array.isArray(rule[tag]) ? rule[tag] : [rule[tag]])
-
-      for (let j = 0; j < patterns.length; j++) {
-        const regex = new RegExp('[\.|\-]' + patterns[j] + '([\.|\-]|$)', 'i')
-
-        if (name.match(regex)) {
-          return {
-            match: tag,
-            waste: name.replace(regex, '$1')
-          }
-        }
-      }
-    }
-
-    return {
+  parse(property, name, multi = false) {
+    const result =  {
       match: null,
       waste: name
     }
+
+    switch (property) {
+      case 'year':
+        const regex = /[\.|\-](\d{4})([\.|\-])?/
+        const matches = name.match(regex)
+
+        if (matches !== null) {
+          result.match = matches[1]
+          result.waste = name.replace(regex, '$2')
+        }
+
+        break
+      default:
+        const rule = Rules[property]
+        const tags = Object.keys(rule)
+
+        for (let i = 0; i < tags.length; i++) {
+          const tag = tags[i]
+          const patterns = (Array.isArray(rule[tag]) ? rule[tag] : [rule[tag]])
+
+          for (let j = 0; j < patterns.length; j++) {
+            const regex = new RegExp('[\.|\-]' + patterns[j] + '([\.|\-]|$)', 'i')
+
+            if (result.waste.match(regex)) {
+              result.match = (multi ? (result.match || []).concat([tag]) : tag)
+              result.waste = result.waste.replace(regex, '$1')
+
+              break
+            }
+          }
+
+          if (!multi && result.match) {
+            break
+          }
+        }
+    }
+
+    return result
   }
 }
 
