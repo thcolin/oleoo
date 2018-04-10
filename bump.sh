ccbfc72e135cef06e26de221ef3fdccbb163b529
thcolin <th.colin3@gmail.com>
2018-04-11 00:13:42 +0200

    Fix bump script

--- bump.sh
+++ bump.sh
@@ -5,14 +5,11 @@
 git stash
 npm run test
 npm --no-git-tag-version version $1
-git add -f build package.json
-git commit -m $1
-git tag v$1
-git push --tags
-git reset --hard HEAD~1
-npm --no-git-tag-version version $1
 git add package.json
 git add package-lock.json
 git commit -m $1
+git tag v$1
+git push --tags
 git push
 git stash pop
+npm publish
