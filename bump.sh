4173dc9b9b8c6de03c847293fc83414be82023a0
thcolin <th.colin3@gmail.com>
2018-04-10 23:58:56 +0200

    Add bump script

---
+++ bump.sh
@@ -0,0 +1,18 @@
+#!env bash
+
+[[ '' == $1 ]] && echo "Please provide version argument: x.x.x" && exit 1
+
+git stash
+npm run test
+npm --no-git-tag-version version $1
+git add -f build package.json
+git commit -m $1
+git tag v$1
+git push --tags
+git reset --hard HEAD~1
+npm --no-git-tag-version version $1
+git add package.json
+git add package-lock.json
+git commit -m $1
+git push
+git stash pop
