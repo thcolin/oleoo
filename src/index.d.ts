e36dfbccd5f608f2381f98b6f2589ea55181dc6c
dezza <402927+dezza@users.noreply.github.com>
2024-03-04 15:15:44 +0100

    Types (#27)
    * Types
    
    * Removed extra type

---
+++ src/index.d.ts
@@ -0,0 +1,34 @@
+type SceneTags = {
+  [key: string]: string | number | number[]
+  original: string,
+  language: string,
+  source: string | null,
+  encoding: string | null,
+  resolution: string | null,
+  dub: string | null,
+  year: number | null,
+  flags: string | null,
+  season: string | null,
+  episode: number | null,
+  episodes: number[],
+  type: string,
+  group: string | null,
+  title: string,
+  generated: string,
+  score: number
+}
+
+type ReleaseOptions = {
+  [key: string]: boolean | string[] | SceneTags | object
+  strict?: boolean,
+  flagged?: boolean,
+  erase?: string[],
+  defaults: SceneTags | object,
+}
+
+declare module 'oleoo' {
+    export function parse(release: string, options?: ReleaseOptions): SceneTags;
+    export function guess(release: string, options?: ReleaseOptions): SceneTags;
+    export function stringify(release: string, options?: ReleaseOptions): string;
+    export const rules: string;
+}
